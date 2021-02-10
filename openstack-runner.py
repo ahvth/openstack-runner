81b051d276d0cb3d90b34fb3c44cf6bf80140d42
ahvth <andrewaslan@outlook.com>
2021-02-10 22:03:17 +0100

    implement basic tcp listener

--- openstack-runner.py
+++ openstack-runner.py
@@ -1,4 +1,36 @@
-import time
-on = 1
-while on is 1:
-    time.sleep(5)
+# TODO: implement return codes from server
+
+import socket
+import json
+import webob 
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket.bind(('localhost', 8090))
+
+# become a server socket, maximum 5 connections
+serversocket.listen(5) 
+
+
+def extract_json_objects(text, decoder=json.JSONDecoder()):
+    pos = 0
+    while True:
+        match = text.find('{', pos)
+        if match == -1:
+            break
+        try:
+            result, index = decoder.raw_decode(text[match:])
+            yield result
+            pos = match + index
+        except ValueError:
+            pos = match + 1
+
+while True:
+    connection, address = serversocket.accept()
+    buf = connection.recv(1024)
+    if len(buf) > 0:
+        u = buf.decode('utf-8')
+        result = []
+        for obj in extract_json_objects(u):
+            data = obj
+        print(data)
