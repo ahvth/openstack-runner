3f8e91025b3c059d1e871e2dad22ba1b2a9f95d5
ahvth <andrewaslan@outlook.com>
2021-02-11 10:45:40 +0100

    add response codes

--- openstack-runner.py
+++ openstack-runner.py
@@ -1,4 +1,4 @@
-# TODO: implement return codes from server
+# test connection with `curl --header "Content-Type: application/json" --request POST --data '{"username":"xyz","password":"xyz"}' http://localhost:8090` 
 
 import socket
 import json
@@ -25,11 +25,23 @@ def extract_json_objects(text, decoder=json.JSONDecoder()):
             pos = match + 1
 
 while True:
+    data = ''
     connection, address = serversocket.accept()
     buf = connection.recv(1024)
-    if len(buf) > 0:
-        u = buf.decode('utf-8')
-        result = []
-        for obj in extract_json_objects(u):
-            data = obj
-        print(data)
+    try:
+        if len(buf) > 0:
+            u = buf.decode('utf-8')
+            result = []
+            for obj in extract_json_objects(u):
+                    data = obj
+            print(data)
+        response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\nJSON data posted. Disconnecting.\n"
+        encResponse = response.encode('utf-8')
+        connection.send(encResponse)
+        connection.close()
+    except:
+        response = "HTTP/1.1 400 Bad Request\nContent-Type: text/html\n\nMalformed request.\n"
+        encResponse = response.encode('utf-8')
+        connection.send(encResponse)
+        connection.close()
+
