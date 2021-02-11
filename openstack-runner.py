a671b91c6e9c11e4a6c1fc3d0c986437a394179a
ahvth <andrewaslan@outlook.com>
2021-02-11 15:11:48 +0100

    add basic input validation

--- openstack-runner.py
+++ openstack-runner.py
@@ -24,24 +24,33 @@ def extract_json_objects(text, decoder=json.JSONDecoder()):
         except ValueError:
             pos = match + 1
 
-while True:
+def get_connections(serversocket):
     data = ''
     connection, address = serversocket.accept()
     buf = connection.recv(1024)
-    try:
-        if len(buf) > 0:
-            u = buf.decode('utf-8')
-            result = []
-            for obj in extract_json_objects(u):
-                    data = obj
-            print(data)
-        response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\nJSON data posted. Disconnecting.\n"
-        encResponse = response.encode('utf-8')
-        connection.send(encResponse)
-        connection.close()
-    except:
-        response = "HTTP/1.1 400 Bad Request\nContent-Type: text/html\n\nMalformed request.\n"
-        encResponse = response.encode('utf-8')
-        connection.send(encResponse)
-        connection.close()
+    if len(buf) > 0:
+        u = buf.decode('utf-8')
+        json_objects = []
+        for obj in extract_json_objects(u):
+            json_objects.append(obj)
+        if len(json_objects) == 1:
+            response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\nJSON data posted. Disconnecting.\n"
+            print(json_objects[0])
+            encResponse = response.encode('utf-8')
+            connection.send(encResponse)
+            connection.close()
+            return data       
+        else:
+            response = "HTTP/1.1 400 Bad Request\nContent-Type: text/html\n\nMalformed request. Too many JSON arrays provided.\n"
+            print("Got bad request.")
+            enc_response = response.encode('utf-8')
+            connection.send(enc_response)
+            connection.close()
+
+def process_data():
+    pass
+
+while True:
+    get_connections(serversocket)
+
 
